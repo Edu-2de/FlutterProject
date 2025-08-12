514d1af69a844e3e610c977be19694c189aef961
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-12 20:38:28 -0300

    fix: Remove unnecessary line and add initial structure for register method in AuthController

--- FlutterPoject/backend/src/controllers/authController.ts
+++ FlutterPoject/backend/src/controllers/authController.ts
@@ -9,7 +9,6 @@ export class AuthController {
   static login = async (req: Request, res: Response): Promise<void> => {
     try {
       const { email, password } = req.body;
-
       if (!email || !password) {
         res.status(400).json({
           success: false,
@@ -66,5 +65,28 @@ export class AuthController {
         code: 'SERVER_ERROR',
       });
     }
+  };
+  static register = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { first_name, last_name, email, phone, password } = req.body;
+      if (!first_name || !email || !phone || !password) {
+        res.status(400).json({
+          success: false,
+          message: 'Email or password is missing',
+          code: 'MISSING_CREDENTIALS',
+        });
+        return;
+      }
+
+      const checkEmailExists = await pool.query(`SELECT * from users WHERE email = $1`, [email]);
+      if (checkEmailExists.rows.length !== 0) {
+        res.status(401).json({
+          success: false,
+          message: 'Invalid credentials',
+          code: 'INVALID_CREDENTIALS',
+        });
+        return;
+      }
+    } catch (error) {}
   };
 }
