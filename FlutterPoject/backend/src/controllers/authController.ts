5611de297e49a4611f468ac549530ed5edffe6da
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-12 12:38:35 -0300

    feat: Implement login functionality with email and password validation in AuthController

--- FlutterPoject/backend/src/controllers/authController.ts
+++ FlutterPoject/backend/src/controllers/authController.ts
@@ -5,3 +5,23 @@ import pool from '../database/connection';
 
 //coming from .env
 const JWT_SECRET = process.env.JWT_SECRET || '';
+
+export class AuthController {
+  static login = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { email, password } = req.body;
+      //error if email or password is missing
+      if (!email || !password) {
+        res.status(400).json({ message: 'Email or password is missing' });
+        return;
+      }
+
+      const checkResult = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
+      //error if the db query does not return any data
+      if (checkResult.rows.length === 0) {
+        res.status(400).json({ message: 'This user does not exist' });
+        return;
+      }
+    } catch (error) {}
+  };
+}
