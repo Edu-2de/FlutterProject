cf3cc76786703c6420a99942a2296e032b03d409
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-12 12:42:26 -0300

    fix: Improve error handling in login method with detailed response structure

--- FlutterPoject/backend/src/controllers/authController.ts
+++ FlutterPoject/backend/src/controllers/authController.ts
@@ -3,23 +3,30 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import pool from '../database/connection';
 
-//coming from .env
 const JWT_SECRET = process.env.JWT_SECRET || '';
 
 export class AuthController {
   static login = async (req: Request, res: Response): Promise<void> => {
     try {
       const { email, password } = req.body;
-      //error if email or password is missing
+
       if (!email || !password) {
-        res.status(400).json({ message: 'Email or password is missing' });
+        res.status(400).json({
+          success: false, 
+          message: 'Email or password is missing' ,
+          code: 'MISSING_CREDENTIALS'
+        });
         return;
       }
 
       const checkResult = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
-      //error if the db query does not return any data
+
       if (checkResult.rows.length === 0) {
-        res.status(400).json({ message: 'This user does not exist' });
+        res.status(401).json({ 
+          success: false,
+          message: 'Invalid credentials',
+          code: 'INVALID_CREDENTIALS' 
+        });
         return;
       }
     } catch (error) {}
